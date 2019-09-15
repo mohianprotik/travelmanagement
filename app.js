6e8f3d89d68c04b27bff6905c2af4d9843357cd3
mohianprotik <54508053+mohianprotik@users.noreply.github.com>
2019-09-16 02:34:03 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+  links.forEach(link => {
+    link.classList.toggle("fade");
+                });
+});
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navigation").style.padding = "10px 10px";
+    
+  } else {
+    document.getElementById("navigation").style.padding = "20px 10px";
+    
+  }
+}
